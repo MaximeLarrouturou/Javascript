920c512348d884396140aac36e032791deeb6b27
MaximeLarrouturou <larrouturoumaxime@gmail.com>
2019-03-14 00:16:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Code JavaScript ici
+
+//Je déclenche une alerte
+alert('Mon alert est bien déclenché')
+// Afficher un message dans la console du navigateur (F12 au clavier) pour windows ou (cmd R) pour Mac
+console.log("Mon message est bien affiché");
+
+//Variable
+var num = 1;
+console.log(num);
+
+// on note aucune valeur est indiqué à la variable chiffre
+var chiffre;
+console.log(chiffre );
+console.log(chiffre + num);
+
+//la valeur de notre variable chiffre à changé, pas besoin de définir à nouveau la variable avec var
+chiffre = 10;
+console.log(chiffre + num);
